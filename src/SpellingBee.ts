7863c917bdb065d0f91c0ab5a54e02019e8e77f2
Zobeir Hamid <zobeirhamid@berkeley.edu>
2021-03-26 16:26:05 +0100

    Reverst back to JS

--- src/SpellingBee.ts
+++ src/SpellingBee.ts
@@ -1,4 +1,5 @@
 import * as actions from "./actions";
 import * as generators from "./generators";
 import * as validators from "./validators";
+
 export default { ...actions, ...generators, ...validators };
